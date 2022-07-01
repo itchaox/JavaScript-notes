47622d92ca9ae66f5fa3cfa276ee07c4aa0041ef
itchaox <wangchao2423@gmail.com>
2022-07-01 12:43:01 +0800

    update

---
+++ 独立练习 JS 代码/03_非空判断/01_&& 非空判断.js
@@ -0,0 +1,11 @@
+/*
+ * @Desc: 
+ * @Version: v1.00
+ * @Author: wc
+ * @Date: 2022-06-29 17:08:09
+ * @LastEditors: wc
+ * @LastEditTime: 2022-06-29 17:10:16
+ */
+
+let arr = null
+console.log('arr.length',arr && arr.length)
