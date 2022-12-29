3a8b981622ab0175de3fc369792c77af7b62321d
王超 <wangchao2423@gmail.com>
2022-12-29 15:16:53 +0800

    feat(深拷贝): 实现深拷贝函数

--- JavaScript-code（学习练习代码）/37_深拷贝-事件总线/02_实现深拷贝函数v1-基本实现.js
+++ JavaScript-code（学习练习代码）/37_深拷贝-事件总线/02_实现深拷贝函数v1-基本实现.js
@@ -4,24 +4,43 @@
  * @Author: wc
  * @Date: 2022-12-28 15:00:19
  * @LastEditors: wc
- * @LastEditTime: 2022-12-28 15:22:10
+ * @LastEditTime: 2022-12-29 10:52:26
  */
+function isObject(value) {
+  const valueType = typeof value;
+  return value !== null && valueType === "object";
+}
 
-function deepClone() {
+function deepClone(sourceValue) {
+  const isFunction = typeof sourceValue === 'function'
 
+  // 直接返回
+  if (!isObject(sourceValue) || isFunction ) {
+    return sourceValue;
+  }
+
+  // 对象类型，遍历获取拷贝新对象
+  const newValue = {};
+  for (const key in sourceValue) {
+    newValue[key] = deepClone(sourceValue[key]);
+  }
+
+  return newValue;
 }
 
 let obj = {
-  name: 'it',
-  age: 23
-}
+  name: "it",
+  age: 23,
+  friend: {
+    name: 'itchao',
+    age: 22
+  }
+};
+let obj1 = obj
 
-const new1 = deepClone(obj)
+const new1 = deepClone(obj);
+obj.friend.name = '222'
 
-console.log(new1 === obj);
+console.log(obj);
 console.log(new1);
-
-console.log(typeof obj );
-console.log(typeof []);
-
-console.log(Symbol('11111111').description);
+console.log(obj1);
