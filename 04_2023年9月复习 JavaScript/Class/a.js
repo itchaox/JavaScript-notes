b23295a8ee2bb4addadb6703e4df12989d453d88
wangchao <wangchao2423@gmail.com>
2024-04-15 15:14:47 +0800

    feat: 修改目录

---
+++ 04_2023年9月复习 JavaScript/Class/a.js
@@ -0,0 +1,9 @@
+/*
+ * @Version    : v1.00
+ * @Author     : wangchao
+ * @Date       : 2024-04-15 14:27
+ * @LastAuthor : wangchao
+ * @LastTime   : 2024-04-15 14:27
+ * @desc       :
+ */
+console.log("test");
