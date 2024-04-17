4b23e69a5ad721a3d20bc6fefbb4949f75a75622
wangchao <wangchao2423@gmail.com>
2024-04-17 16:46:47 +0800

    feat: es 12

---
+++ 05_重学 JavaScript 2024-04/ES11/05_fo in 标准化.js
@@ -0,0 +1,12 @@
+/*
+ * @Version    : v1.00
+ * @Author     : wangchao
+ * @Date       : 2024-04-17 16:33
+ * @LastAuthor : wangchao
+ * @LastTime   : 2024-04-17 16:36
+ * @desc       :
+ */
+
+// for of 获取数组中的每个对象
+
+// for in 获取对象的 key
